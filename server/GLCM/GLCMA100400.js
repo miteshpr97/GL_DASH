81ef1c0b82e2f37e88ed85fbb639553770f0c770
TauqeerDevX <hasantauqeer20@gmail.com>
2024-12-31 17:11:46 +0530

    Added api to save update and get menu

---
+++ server/GLCM/GLCMA100400.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const dbUtil = require("../DBCall/DBUtil");
+const router = express.Router();
+
+// Adding a menu
+router.post("/", async (req, res) => {
+    console.log("Add or update menu");
+  
+    const strParaMeter = {
+      MODULE_CD: req.body.MODULE_CD,
+      MENU_CD: req.body.MENU_CD,
+      MENU_NM: req.body.MENU_NM,
+      PAGE_CD: req.body.PAGE_CD,
+      PAGE_NM: req.body.PAGE_NM,
+      PAGE_ID: req.body.PAGE_ID,
+      RSTATUS: req.body.RSTATUS,
+      ICON_PAGE: req.body.ICON_PAGE,
+      ICON_MODULE: req.body.ICON_MODULE,
+      ICON_MENU: req.body.ICON_MENU,
+      PAGE_LNK: req.body.PAGE_LNK,
+      REG_BY: req.body.REG_BY,
+      ACTIVE_YN: req.body.ACTIVE_YN,
+    };
+  
+    try {
+      const result = await dbUtil.dbUtil_Temp.Save_SP(
+        "SP_GLCMA100400_01",
+        strParaMeter
+      );
+      console.log("Result", result);
+      res.status(201).json({ message: "Menu added successfully", result });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .send({ error: "An error occurred while adding menu." });
+    }
+});
+
+// Getting menu
+router.post("/get", async (req, res) => {
+    console.log("Getting menu");
+
+    const strParaMeter = {
+        MODULE_CD: req.body.MODULE_CD,
+        SERACH_TEXT: req.body.SERACH_TEXT,
+        REG_BY: req.body.REG_BY,
+        };
+  
+    try {
+      const result = await dbUtil.dbUtil_Temp.Select_SP(
+        "SP_GLCMA100400_02",
+        strParaMeter
+    );
+      console.log("Result", result);
+      res.status(200).json(result);
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .send({ error: "An error occurred while getting menu." });
+    }
+});
+
+module.exports = router;
--- server/StartUp/glRoute.js
+++ server/StartUp/glRoute.js
@@ -7,6 +7,7 @@ const apiAccessRight = require("../GLCM/GLCM1004");
 const apiUserCreation = require("../GLCM/GLCMA100100");
 const apiPageGLCMA100200 = require("../GLCM/GLCMA100200");
 const apiPageGLCMA100300 = require("../GLCM/GLCMA100300");
+const apiPageGLCMA100400 = require("../GLCM/GLCMA100400");
 const apiPageGLAMT100100 = require("../GLCM/GLAMT100100");
 const apiPageGLAMA100100 = require("../GLCM/GLAMA100100");
 const apiOfCommonDropdown = require("../GLCM/GLCWP100100");
@@ -21,6 +22,7 @@ module.exports = function (app) {
   app.use("/api/UserCreation", apiUserCreation);
   app.use("/api/GLCMA100200", apiPageGLCMA100200);
   app.use("/api/GLCMA100300", apiPageGLCMA100300);
+  app.use("/api/GLCMA100400", apiPageGLCMA100400);
   app.use("/api/GLAMT100100", apiPageGLAMT100100);
   app.use("/api/GLAMA100100", apiPageGLAMA100100);
   app.use("/api/GLCWP100100", apiOfCommonDropdown);
